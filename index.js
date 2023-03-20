2ac92b48bb200d19059aa88ad168e33bf65c8edb
Revan Ahmad Zayyan <revanzayyan@gmail.com>
2023-03-21 00:36:36 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// function
+function hitungBmi() {
+    let beratBadan = document.getElementById("berat-badan").value;
+    let tinggiBadan = document.getElementById("tinggi-badan").value/100;
+
+    let bmi = beratBadan / (tinggiBadan**2);
+    
+    //conditional
+
+    if (bmi < 18.5) {
+        document.getElementById("hasil-bmi").innerHTML=("Kamu kekurangan berat badan")
+    }
+    
+
+
+}
+
+
+
+
