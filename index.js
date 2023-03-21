b79958837e65656aba6bfd4fd0c5704b2be03924
Revan Ahmad Zayyan <revanzayyan@gmail.com>
2023-03-21 16:00:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-// function
-function hitungBmi() {
-    let beratBadan = document.getElementById("berat-badan").value;
-    let tinggiBadan = document.getElementById("tinggi-badan").value/100;
 
-    let bmi = beratBadan / (tinggiBadan**2);
 
+// function tombol submit
+function hitungBmi() { 
+
+    let jenisKelamin = document.getElementsByName("jenis-kelamin")
+let beratBadan = document.getElementById("berat-badan").value;
+let tinggiBadan = document.getElementById("tinggi-badan").value/100;
+
+//rumus bmi
+let bmi = beratBadan / (tinggiBadan**2);    
+    
     //conditional
-
     if (bmi < 18.5) {
         document.getElementById("hasil-bmi").innerHTML=("Kamu kekurangan berat badan")
     }
     
+    console.log(tinggiBadan);
+
+}
 
 
+
+function reset() {
+    location.reload()
 }
 
 
